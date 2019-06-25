d3c66146f337b2d456999d8599da749cb6a7948e
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2019-06-25 19:39:10 -0400

    Updates

---
+++ precache-manifest.4c745419bab9a13fad461c18fea6332a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e16ae0779eda8f0bcb427b36b6792ae",
+    "url": "/hatchways/index.html"
+  },
+  {
+    "revision": "2541068fbccb54e8f6fd",
+    "url": "/hatchways/static/css/main.80ffe13b.chunk.css"
+  },
+  {
+    "revision": "406d4a3ffe042cb4f22a",
+    "url": "/hatchways/static/js/2.736acf68.chunk.js"
+  },
+  {
+    "revision": "2541068fbccb54e8f6fd",
+    "url": "/hatchways/static/js/main.8e4363c5.chunk.js"
+  },
+  {
+    "revision": "edc4a9b76e3d15a307a0",
+    "url": "/hatchways/static/js/runtime~main.91cf8ce4.js"
+  }
+]);
